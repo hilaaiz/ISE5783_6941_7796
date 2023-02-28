47b8afe8d3b8b1b52d221bbbff50423b648b13ee
מוריה מוסקוביץ <moskovic@g.jct.ac.il>
2023-02-28 19:40:17 +0200

    pull

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -3,7 +3,52 @@ package primitives;
 public class Vector extends Point {
 
 
-    public Vector(Double3 xyz) {
+    public Vector(double x ,double y, double z ){
+        super(x,y,z);
+    }
+
+    Vector(Double3 xyz) {
         super(xyz);
+    }
+
+
+    /**
+     *Addition of two vectors
+     * @param myVector
+     * @return
+     */
+    public Vector add(Vector myVector){
+        return new Vector(xyz.add(myVector.xyz));
+    }
+
+    /**
+     * Vector multiplication in scalar
+     * @param num
+     * @return
+     */
+    public Vector scale(double num){
+        return new Vector(xyz.scale(num));
+    }
+
+
+    /**
+     * A scalar force between two vectors
+     * @param myVector
+     * @return
+     */
+    public double dotProduct(Vector myVector){
+        return xyz.d1*myVector.xyz.d1+xyz.d2*myVector.xyz.d2+xyz.d3*myVector.xyz.d3;
+    }
+
+
+    /**
+     * A vector force between two vectors
+     * @param myVector
+     * @return
+     */
+    public Vector crossProduct(Vector myVector){
+        return new Vector(xyz.d2*myVector.xyz.d3-xyz.d3*myVector.xyz.d2,
+                -(xyz.d1*myVector.xyz.d3-xyz.d3*myVector.xyz.d1),
+                xyz.d1*myVector.xyz.d2-xyz.d2*myVector.xyz.d1);
     }
 }
