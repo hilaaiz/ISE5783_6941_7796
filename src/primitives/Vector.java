1907e52a996ec5635b6314fa3b588108dc1b3d2d
hilaaiz <haizenba@g.jct.ac.il>
2023-03-15 13:27:51 +0200

    refactor vector and other

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -51,4 +51,20 @@ public class Vector extends Point {
                 -(xyz.d1*myVector.xyz.d3-xyz.d3*myVector.xyz.d1),
                 xyz.d1*myVector.xyz.d2-xyz.d2*myVector.xyz.d1);
     }
+
+    /**
+     * return normalize vector
+     * @param o
+     * @return
+     */
+    public Vector normalize(){
+        double dis= distance(this);
+        return new Vector(xyz.d1/dis,xyz.d2/dis,xyz.d3/dis);
+    }
+
+    //return if the objects are equals
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
 }
