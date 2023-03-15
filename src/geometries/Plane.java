9a7dea65f851bb4bbbbad237c3ed7cd9e9aab86e
hilaaiz <haizenba@g.jct.ac.il>
2023-03-15 14:30:24 +0200

    plane

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -1,9 +1,27 @@
 package geometries;
 
 import primitives.Point;
+import primitives.Vector;
 
 public class Plane {
-    public Plane(Point vertex0, Point vertex1, Point vertex2) {
 
+    private Point P0;
+    private Vector normal;
+
+    public Plane(Point vertex0, Point vertex1, Point vertex2) {
+        normal= null;
+        P0= vertex0;
+    }
+
+    public Plane (Point p,Vector v ){
+        P0= p;
+        normal=v.normalize();
+    }
+
+    public Vector getNormal() {
+        return normal;
+    }
+    public Vector getNormal(Point p){
+        return normal;
     }
 }
